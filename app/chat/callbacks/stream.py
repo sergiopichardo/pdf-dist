e3aa9e8dae89ebbbf6aae3852b73fabab82563f2
Sergio Pichardo <sergiopichardo@users.noreply.github.com>
2024-01-11 19:51:14 -0600

    feat: make sure only the streaming model is used when streaming text

--- app/chat/callbacks/stream.py
+++ app/chat/callbacks/stream.py
@@ -3,12 +3,22 @@ from langchain.callbacks.base import BaseCallbackHandler
 class StreamingHandler(BaseCallbackHandler):
     def __init__(self, queue):
         self.queue = queue
+        self.streaming_run_ids = set()
+
+    def on_chat_model_start(self, serialized, messages, run_id, **kwargs):
+        # print("serialized:", serialized)
+        # print("run_id:", run_id)
+        if serialized["kwargs"]["streaming"]:
+            # print("This is a streaming model! I should listen to events with a run_id of:", run_id)
+            self.streaming_run_ids.add(run_id)
 
     def on_llm_new_token(self, token, **kwargs):
         self.queue.put(token)
 
-    def on_llm_end(self, response, **kwargs):
-        self.queue.put(None)
+    def on_llm_end(self, response, run_id, **kwargs):
+        if run_id in self.streaming_run_ids:
+            self.queue.put(None)
+            self.streaming_run_ids.remove(run_id)
 
     def on_llm_error(self, error, **kwargs):
         self.queue.put(None)
